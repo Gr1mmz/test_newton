8d40ab920abcce69c00a42ee76efb5c38a127b23
Gr1mmz <admin@kraevskiy-web.ru>
2022-06-04 18:04:36 +0800

    add responsive styles

--- components/MainApp/MainApp.tsx
+++ components/MainApp/MainApp.tsx
@@ -8,9 +8,21 @@ import GalleryItem from '../GalleryItem/GalleryItem';
 const MainApp = observer(() => {
   const headingText = (gallery.filter === 'all') ? 'Photo gallery' : 'Favorite photos';
   return (
-    <Box p='3em' w='100%' maxH='100%' sx={{ overflowY: 'scroll' }}>
+    <Box p='3em' w='100%' maxH='100%'
+         sx={{
+           overflowY: 'scroll',
+           '@media screen and (max-width: 425px)': {
+             padding: '2em'
+           }
+         }}
+    >
       <Heading>{headingText}</Heading>
-      <SimpleGrid minChildWidth='300px' spacing='2em' mt='2em'>
+      <SimpleGrid minChildWidth='300px' spacing='2em' mt='2em'
+                  sx={{
+                    '@media screen and (max-width: 425px)': {
+                      gridTemplateColumns: 'repeat(auto-fit, minmax(240px, 1fr))'
+                    }
+                  }}>
         {gallery.getFilteredItems()?.map(item => <GalleryItem key={item.id} item={item}/>)}
       </SimpleGrid>
     </Box>
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,31 +1,47 @@
-import React from 'react';
-import {Box, Button, Flex} from '@chakra-ui/react';
+import React, {useState} from 'react';
+import {Box, Button, Flex, IconButton} from '@chakra-ui/react';
 import gallery from '../../store/gallery';
+import { AiOutlineDoubleRight } from 'react-icons/ai';
 
 const Sidebar = () => {
+  const [isSidebarShow, setSidebarShow] = useState(false);
+
   return (
     <Box bg='#1e1830' w='20%' maxW='360px' minW='250px' p='3em 2em'
          sx={{
+           position: 'relative',
            borderTopRightRadius: '30px',
            borderBottomRightRadius: '30px',
-         }}
-    >
+           transition: 'left 0.3s ease 0s',
+           '@media screen and (max-width: 768px)': {
+             position: 'absolute',
+             height: '100vh',
+             left: isSidebarShow ? '0' : '-250px',
+             zIndex: '100'
+           }
+         }}>
+      <IconButton aria-label='Show sidebar' onClick={() => setSidebarShow(!isSidebarShow)} icon={<AiOutlineDoubleRight/>}
+                  sx={{
+                    position: 'absolute',
+                    right: '-30px',
+                    backgroundColor: '#1e1830',
+                    display: 'none',
+                    ':hover': {backgroundColor: '#1e1830',},
+                    ':hover, :focus': {boxShadow: 'none',},
+                    '@media screen and (max-width: 768px)': {display: 'flex'}
+                  }}/>
       <Flex direction='column' gap='0.5em'>
         <Button variant='ghost' colorScheme='purple' color='#fff' onClick={() => gallery.setFilter('all')}
                 sx={{
                   justifyContent: 'flex-start',
-                  ':hover': {
-                    color: 'purple.900'
-                  }
+                  ':hover': {color: 'purple.900'}
                 }}>
           Gallery
         </Button>
         <Button variant='ghost' colorScheme='purple' color='#fff' onClick={() => gallery.setFilter('favorite')}
                 sx={{
                   justifyContent: 'flex-start',
-                  ':hover': {
-                    color: 'purple.900'
-                  }
+                  ':hover': {color: 'purple.900'}
                 }}>
           Favorites
         </Button>
