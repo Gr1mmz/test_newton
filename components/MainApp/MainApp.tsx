852308207a4925ee7826be9320be84eb97702ac6
Gr1mmz <admin@kraevskiy-web.ru>
2022-06-04 18:37:37 +0800

    add message if app have no items to show

--- components/MainApp/MainApp.tsx
+++ components/MainApp/MainApp.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {observer} from 'mobx-react-lite';
 import {Box, Heading, SimpleGrid} from '@chakra-ui/react';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
 
 import gallery from '../../store/gallery';
 import GalleryItem from '../GalleryItem/GalleryItem';
@@ -20,7 +21,14 @@ const MainApp = observer(() => {
                       gridTemplateColumns: 'repeat(auto-fit, minmax(240px, 1fr))'
                     }
                   }}>
-        {gallery.getFilteredItems()?.map(item => <GalleryItem key={item.id} item={item}/>)}
+        {gallery.getFilteredItems().length
+          ? gallery.getFilteredItems()?.map(item => <GalleryItem key={item.id} item={item}/>)
+          : <Box bg='whiteAlpha.50' p='1em  2em' borderRadius='lg' textAlign='center' justifySelf='center'
+                 display='flex' alignItems='center' gap='0.5em' fontSize='0.9em' >
+              <AiOutlineInfoCircle/>
+              There is nothing here yet
+            </Box>
+        }
       </SimpleGrid>
     </Box>
   );
